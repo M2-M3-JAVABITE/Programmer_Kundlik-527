076b04723389893b30a78a09eb7198eedcdc5c9b
M2_M3_JAVABITE <156278685+M2-M3-JAVABITE@users.noreply.github.com>
2024-02-07 18:37:17 +0530

    programm to print String variable

---
+++ PrintStringValue.java
@@ -0,0 +1,16 @@
+class PrintStringValue
+
+{
+
+public static void main(String[]psv)
+{
+String name ="Akash";
+System.out.println(name);
+
+}
+
+}
+/*output:
+1:Compile time: Success
+2:RunTime :Success :Akash;
+*/
